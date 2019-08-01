4d225945adade459ba2d2d82a684e0420d247fcc
pgilfillan <omaly321@hotmail.com>
2019-08-02 08:11:20 +1000

    Updates to PUT form

--- src/components/storage-upload/StoragePUT.js
+++ src/components/storage-upload/StoragePUT.js
@@ -2,8 +2,15 @@ import React from 'react';
 import Divider from '@material-ui/core/Divider';
 import { FormControl, FormLabel, FormControlLabel, Radio, 
          RadioGroup, TextField, Button, InputLabel, Select, MenuItem } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+
+}));
 
 function StoragePUT() {
+  const classes = useStyles();
+
   // State
   const [typeValue, setTypeValue] = React.useState('shared');
   const [showDocName, setShowDocName] = React.useState(true);
@@ -25,56 +32,63 @@ function StoragePUT() {
     }
   }
 
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log("/" + dbValue + "/v1/" + typeValue +  "/classes/<class>/documents/<docname>");
+  }
+
   return (
     <div>
-      <FormControl>
-        <InputLabel>Storage Source</InputLabel>
-        <Select
-          value={dbValue}
-          onChange={(e) => {setDBValue(e.target.value)}}
-        >
+      <form onSubmit={handleFormSubmit}>
+        <FormControl>
+          <FormLabel>Storage Source</FormLabel>
+          <Select
+            value={dbValue}
+            onChange={(e) => {setDBValue(e.target.value)}}
+          >
 
-          <MenuItem value="client-shared-storage">client-shared-storage</MenuItem>
-          <MenuItem value="client-user-storage">client-user-storage</MenuItem>
-          <MenuItem value="server-shared-storage">server-shared-storage</MenuItem>
-          <MenuItem value="server-user-storage">server-user-storage</MenuItem>
-        </Select>
+            <MenuItem value="client-shared-storage">client-shared-storage</MenuItem>
+            <MenuItem value="client-user-storage">client-user-storage</MenuItem>
+            <MenuItem value="server-shared-storage">server-shared-storage</MenuItem>
+            <MenuItem value="server-user-storage">server-user-storage</MenuItem>
+          </Select>
 
-        <FormLabel component="legend">Storage Type</FormLabel>
-        <RadioGroup
-          aria-label="storage-type"
-          name="storage-type-choices"
-          value={typeValue}
-          onChange={handleTypeChange}
-          row
-        >
-          <FormControlLabel value="shared" control={<Radio color="primary" />} label="Shared" />
-          <FormControlLabel value="user" control={<Radio color="primary" />} label="User" />
-        </RadioGroup>
+          <FormLabel>Storage Type</FormLabel>
+          <RadioGroup
+            aria-label="storage-type"
+            name="storage-type-choices"
+            value={typeValue}
+            onChange={handleTypeChange}
+            row
+          >
+            <FormControlLabel value="shared" control={<Radio color="primary" />} label="Shared" />
+            <FormControlLabel value="user" control={<Radio color="primary" />} label="User" />
+          </RadioGroup>
 
-        <TextField variant="outlined" label="Class" />
+          <TextField variant="outlined" label="Class" />
+          <Divider />
+
+          <Divider />
+          <RadioGroup
+            value={uploadTypeValue}
+            aria-label="search-type"
+            name="search-type-choices"
+            onChange={handleUploadTypeChange}
+            row
+          >
+            <FormControlLabel value="input" control={<Radio color="primary" />} label="Input Document Name" />
+            <FormControlLabel value="generate" control={<Radio color="primary" />} label="Generate" />
+          </RadioGroup>
+          <TextField variant="outlined" label="Document Name" style={docInputStyle}/>
+
+          <TextField variant="outlined" multiline={true} rows={6} label="Document Body" />
+          <Divider />
+          <Button variant="contained" color="primary" type="submit">
+              Submit
+          </Button>
+        </FormControl>
         <Divider />
-
-        <Divider />
-        <RadioGroup
-          value={uploadTypeValue}
-          aria-label="search-type"
-          name="search-type-choices"
-          onChange={handleUploadTypeChange}
-          row
-        >
-          <FormControlLabel value="input" control={<Radio color="primary" />} label="Input" />
-          <FormControlLabel value="generate" control={<Radio color="primary" />} label="Generate" />
-        </RadioGroup>
-        <TextField variant="outlined" label="Document Name" style={docInputStyle}/>
-
-        <TextField variant="outlined" multiline={true} rows={6} label="Document Body" />
-        <Divider />
-        <Button variant="contained" color="primary">
-            Submit
-        </Button>
-      </FormControl>
-      <Divider />
+      </form>
     </div>
   );
 }
